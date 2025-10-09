f856fb55bbe25ada7835677e04d06554e7ba319d
JuanEscos <142816912+JuanEscos@users.noreply.github.com>
2025-10-09 22:19:21 +0200

    Update flow_events.py

--- flow_events.py
+++ flow_events.py
@@ -51,7 +51,7 @@ except ImportError:
 # ============================== CONFIGURACIÓN GLOBAL ==============================
 
 BASE = "https://www.flowagility.com"
-EVENTS_URL = f"{BASE}/zone/events/past"
+EVENTS_URL = f"{BASE}/zone/events"
 SCRIPT_DIR = Path(__file__).resolve().parent
 
 # Cargar variables de entorno
